dc13f76c4e98e63b36b70c8891d48f0ee55247ec
xyzz <xyzz@XYZZ>
2017-04-10 16:19:44 +0800

    增加作品集接口

---
+++ PersonalPortal/PersonalPortal/Controllers/Project.cs
@@ -0,0 +1,22 @@
+﻿using PersonalPortal.Content;
+using PersonalPortal.Content.Library;
+using PersonalPortal.Models.ResultModels;
+using System.Data;
+
+namespace PersonalPortal.Controllers
+{
+    public class Project
+    {
+        /// <summary>
+        /// 获取作品集项目
+        /// </summary>
+        public ProjectModel GetProject()
+        {
+            return new ProjectModel()
+            {
+                ProjectList = DataConversion.ToEntity<ProjectItem>(DataBase.ExecuteSql<DataTable>("select * from Project where projectType='project'")),
+                GameList = DataConversion.ToEntity<ProjectItem>(DataBase.ExecuteSql<DataTable>("select * from Project where projectType='project'"))
+            };
+        }
+    }
+}
---
+++ PersonalPortal/PersonalPortal/Models/ResultModels/ProjectModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace PersonalPortal.Models.ResultModels
+{
+    public class ProjectModel
+    {
+        /// <summary>
+        /// 应用项目
+        /// </summary>
+        public List<ProjectItem> ProjectList = new List<ProjectItem>();
+        /// <summary>
+        /// 游戏项目(Unity)
+        /// </summary>
+        public List<ProjectItem> GameList = new List<ProjectItem>();
+    }
+
+    public class ProjectItem
+    {
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public string ProjectName { get; set; }
+        /// <summary>
+        /// 项目地址
+        /// </summary>
+        public string ProjectLocal { get; set; }
+    }
+}
