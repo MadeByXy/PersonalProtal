9e40334c0ebabf755a17b4568749ecaf93fa38d5
xyzz <xyzz@XYZZ>
2017-04-12 15:48:27 +0800

    完善组件

--- PersonalPortal/PersonalPortal/Scripts/CodeMirrorInstance.js
+++ PersonalPortal/PersonalPortal/Scripts/CodeMirrorInstance.js
@@ -1,9 +1,22 @@
 ﻿document.write("<link rel='stylesheet' href='/Scripts/CodeMirror/lib/codemirror.css' />");
+document.write("<link rel='stylesheet' href='/Scripts/CodeMirror/fold/foldgutter.css' />");
 document.write("<link rel='stylesheet' href='/Scripts/CodeMirror/theme/mdn-like.css' />");
+document.write("<link rel='stylesheet' href='/Scripts/CodeMirror/lint/lint.css' />");
 
 document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lib/codemirror.js'></script>");
 document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lib/matchbrackets.js'></script>");
 document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lib/active-line.js'></script>");
+
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/fold/foldcode.js'></script>");
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/fold/foldgutter.js'></script>");
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/fold/brace-fold.js'></script>");
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/fold/xml-fold.js'></script>");
+
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lint/lint.js'></script>");
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lint/json-lint.js'></script>");
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lint/css-lint.js'></script>");
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lint/html-lint.js'></script>");
+document.write("<script type='text/javascript' src='/Scripts/CodeMirror/lint/javascript-lint.js'></script>");
 
 document.write("<script type='text/javascript' src='/Scripts/CodeMirror/language/clike.js'></script>");
 document.write("<script type='text/javascript' src='/Scripts/CodeMirror/language/css.js'></script>");
@@ -19,8 +32,11 @@ var Mirror = function (id, language) {
         theme: "mdn-like",
         indentUnit: 4,
         lineNumbers: true,
+        lineWrapping: true,
         styleActiveLine: true,
-        matchBrackets: true
+        matchBrackets: true,
+        foldGutter: true,
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"]
     });
     return editor;
 }
