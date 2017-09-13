0b20cf1c1eb916e318ed73733cb1ee4d0f445994
xyzz <xyzz@XYZZ>
2017-09-13 15:30:14 +0800

    提交Astar算法页面

--- PersonalPortal/PersonalPortal/Scripts/CanvasHelper.js
+++ PersonalPortal/PersonalPortal/Scripts/CanvasHelper.js
@@ -9,6 +9,17 @@
         spacing: 2
     }
 
+    //x节点序号计算横坐标
+    this.x_center = function (index_x, isText) {
+        return index_x * (this.size.width + this.size.spacing) + this.size.spacing + (isText == true ? this.size.width / 2 : 0);
+    }
+
+    //y节点序号计算纵坐标
+    this.y_center = function (index_y, isText) {
+        return index_y * (this.size.height + this.size.spacing) + this.size.spacing + (isText == true ? this.size.height / 2 : 0);
+    }
+
+    //重计算单位长宽
     this.AutoSize = function (size_x, size_y) {
         this.size.width = (this.object.scrollWidth - this.size.spacing) / size_x - this.size.spacing;
         this.size.height = (this.object.scrollHeight - this.size.spacing) / size_y - this.size.spacing;
@@ -17,8 +28,8 @@
     //绘制矩形
     this.Cube = function (index_x, index_y) {
         this.context.fillRect(
-            index_x * (this.size.width + this.size.spacing) + this.size.spacing,
-            index_y * (this.size.height + this.size.spacing) + this.size.spacing,
+            this.x_center(index_x),
+            this.y_center(index_y),
             this.size.width,
             this.size.height);
     }
@@ -29,18 +40,37 @@
         this.context.textAlign = 'center';
         this.context.textBaseline = 'middle';
 
-        //重计算文字位置使文字位于背景中心
         this.context.fillText(
             text,
-            index_x * (this.size.width + this.size.spacing) + this.size.spacing + this.size.width / 2,
-            index_y * (this.size.height + this.size.spacing) + this.size.spacing + this.size.height / 2);
+            this.x_center(index_x, true),
+            this.y_center(index_y, true));
     }
 
     //绘制线段
     this.Line = function (move_x, move_y, line_x, line_y) {
-        this.context.moveTo(move_x, move_y);
-        this.context.lineTo(line_x, line_y);
+        this.context.beginPath();
+
+        this.context.moveTo(
+            this.x_center(move_x, true),
+            this.y_center(move_y, true));
+
+        this.context.lineTo(
+            this.x_cneter(line_x, true),
+            this.y_center(line_y, true));
         this.context.stroke();
+        this.context.closePath();
+    }
+
+    //绘制线段组
+    this.LineArray = function (indexArray, style) {
+        this.context.beginPath();
+        for (var i = 0; i < indexArray.length; i++) {
+            this.context.lineTo(this.x_center(indexArray[i].x, true), this.y_center(indexArray[i].y, true))
+        }
+
+        this.context.strokeStyle = style || 'black';
+        this.context.stroke();
+        this.context.closePath();
     }
 
     //清理画布
