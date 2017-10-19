3a4fcb619e6338f5cd5bf280868aea5ba2219f3b
xyzz <xyzz@XYZZ>
2017-10-19 15:08:35 +0800

    弃用cookie改用localStorage进行前端储存

--- PersonalPortal/PersonalPortal/Scripts/Cookie.js
+++ PersonalPortal/PersonalPortal/Scripts/Cookie.js
@@ -1,33 +1,28 @@
 ﻿//写入cookies
 function setCookie(name, value) {
-    if (getCookie(name) != null) { delCookie(name); }
     name = encodeURIComponent(encodeURIComponent(name));
-    var exp = new Date();
-    exp.setTime(exp.getTime() + 60 * 60 * 1000);
-    try { var temp = JSON.stringify(value); value = temp; } catch (e) { value = encodeURIComponent(encodeURIComponent(value)); }
-    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+    delCookie(name);
+    try {
+        value = JSON.stringify(value);
+    } catch (e) {
+        value = encodeURIComponent(encodeURIComponent(value));
+    }
+    localStorage.setItem(name, value);
 }
 
 //读取cookies
 function getCookie(name) {
     name = encodeURIComponent(encodeURIComponent(name));
-    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-    if (arr = document.cookie.match(reg)) {
-        var result = unescape(arr[2]);
-        try {
-            return JSON.parse(result);
-        } catch (e) {
-            return decodeURIComponent(decodeURIComponent(result));
-        }
+    var result = localStorage.getItem(name);
+    try {
+        return JSON.parse(result);
+    } catch (e) {
+        return decodeURIComponent(decodeURIComponent(result));
     }
-    else { return null; }
+    return result;
 }
 
 //删除cookies
 function delCookie(name) {
-    var exp = new Date();
-    exp.setTime(exp.getTime() - 1);
-    var cval = getCookie(name);
-    if (cval != null)
-        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    localStorage.removeItem(name);
 }
