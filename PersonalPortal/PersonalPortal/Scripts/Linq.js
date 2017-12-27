49cd57a48c68b4731f10b4e8f70806776235daed
xyzz <xyzz@XYZZ>
2017-12-27 17:04:42 +0800

    完善linq功能

--- PersonalPortal/PersonalPortal/Scripts/Linq.js
+++ PersonalPortal/PersonalPortal/Scripts/Linq.js
@@ -1,73 +1,131 @@
-﻿//返回数组第一项匹配内容, 如果没有匹配, 则返回null
-Array.prototype.first = function (func, key) {
-    var result = null;
-
-    $.each(this, function (index, item) {
-        if (func(item)) {
-            result = item;
-            return false;
+﻿//返回第一项匹配的内容，如果没有匹配成功，返回默认值
+Array.prototype.first = function (func, defaultValue) {
+    for (var i = 0; i < this.length; i++) {
+        if (func(this[i])) {
+            return key || defaultValue;
         }
-    })
-
-    if (key != null && result != null) {
-        result = result[key];
     }
-
-    return result;
+    return defaultValue;
 }
 
-//返回数组第一项匹配内容的序号, 如果没有匹配, 则返回-1
+//返回第一项匹配的内容的序号，如果没有匹配成功，返回-1
 Array.prototype.firstIndex = function (func) {
-    var result = -1;
-
-    $.each(this, function (index, item) {
-        if (func(item)) {
-            result = index;
-            return false;
+    for (var i = 0; i < this.length; i++) {
+        if (func(this[i])) {
+            return i;
         }
-    })
-
-    return result;
+    }
+    return -1;
 }
 
-//按照条件对数组进行排序
+//按照指定条件对数组进行排序（由小到大）
 Array.prototype.orderBy = function (func) {
-    return this.sort(function (before, next) {
+    var array = this.concat();
+    return array.sort(function (before, next) {
         return func(before) - func(next);
     });
 }
 
 //返回数组是否包含指定内容
-Array.prototype.contains = function (func) {
-    return this.firstIndex(func) != -1;
+Array.prototype.contains = function (text) {
+    return this.any(item => item == text);
 }
 
-//将序列中的每个元素投影到新表中
+//确定数组是否所有元素都满足条件
+Array.prototype.all = function (func) {
+    for (var i = 0; i < this.length; i++) {
+        if (!func(this[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//确定数组是否有任意元素满足条件
+Array.prototype.any = function (func) {
+    for (var i = 0; i < this.length; i++) {
+        if (func(this[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//数据去重
+Array.prototype.distinct = function (func) {
+    var array = [];
+    func = func || (item => item);
+    for (var i = 0; i < this.length; i++) {
+        if (!array.any(item => func(item) == func(this[i]))) {
+            array.push(this[i]);
+        }
+    }
+    return array;
+}
+
+//返回数组内元素最大值
+Array.prototype.max = function (func) {
+    var max = null;
+    func = func || (item => item);
+    for (var i = 0; i < this.length; i++) {
+        if (max == null) {
+            max = func(this[i]);
+        } else {
+            max = Math.max(func(this[i]), max);
+        }
+    }
+    return max;
+}
+
+//返回数组内元素最小值
+Array.prototype.min = function (func) {
+    var min = null;
+    func = func || (item => item);
+    for (var i = 0; i < this.length; i++) {
+        if (min == null) {
+            min = func(this[i]);
+        } else {
+            min = Math.min(func(this[i]), min);
+        }
+    }
+    return min;
+}
+
+//返回数组内元素值之和
+Array.prototype.sum = function (func) {
+    var sum = 0;
+    func = func || (item => item);
+    for (var i = 0; i < this.length; i++) {
+        sum += func(this[i]);
+    }
+    return sum;
+}
+
+//将数组中每个元素投影到新表中
 Array.prototype.select = function (func) {
     var array = [];
-    $.each(this, function (index, item) {
-        array.push(func(item, index));
-    })
+    for (var i = 0; i < this.length; i++) {
+        array.push(func(this[i], i));
+    }
     return array;
 }
 
-//基于谓词筛选值序列, 将在谓词函数的逻辑中使用每个元素的索引
+//返回数组中所有符合条件的项
 Array.prototype.where = function (func) {
     var array = [];
-    $.each(this, function (index, item) {
-        if (func(item, index)) {
-            array.push(item);
+    for (var i = 0; i < this.length; i++) {
+        if (func(this[i], i)) {
+            array.push(this[i]);
         }
-    })
+    }
     return array;
 }
 
-//数组转对象
+//将数组转换成对象
 Array.prototype.toObject = function (funcKey, funcValue) {
     var object = {};
-    $.each(this, function (index, item) {
-        object[funcKey(item)] = funcValue(item);
-    })
-
+    for (var i = 0; i < this.length; i++) {
+        object[funcKey(this[i])] = funcValue(this[i]);
+    }
     return object;
 }
